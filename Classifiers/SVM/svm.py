40b55b79744b43c7e540d67d004e100d288930a3
Murtaza <murt202@hotmail.com>
2019-04-26 21:23:39 -0700

    Final code SVM

--- Classifiers/SVM/svm.py
+++ Classifiers/SVM/svm.py
@@ -1,25 +1,20 @@
 import numpy as np
-from numpy import linalg
 import cvxopt
-import cvxopt.solvers
 import pickle as pkl
+import os
+import matplotlib.pyplot as plt
 
 
 def linear_kernel(x1, x2):
     return np.dot(x1, x2)
 
 
-def polynomial_kernel(x, y, p=3):
-    return (1 + np.dot(x, y)) ** p
-
-
-def gaussian_kernel(x, y, sigma=5.0):
-    return np.exp(-linalg.norm(x - y) ** 2 / (2 * (sigma ** 2)))
-
-
 class SVM(object):
 
     def __init__(self, kernel=linear_kernel, C=None):
+        # prevent solver verbose
+        cvxopt.solvers.options['show_progress'] = False
+        # initialize
         self.kernel = kernel
         self.C = C
         if self.C is not None:
@@ -95,47 +90,124 @@ class SVM(object):
         return np.sign(self.project(X))
 
 
+def soft_classifier(c):
+    # get the feature list
+    feature_list = os.listdir('../../Data/Pickle/flatten_features/')
+    feature_list.sort(key=lambda x: int(x.split('_')[2]))
+    feature_list = np.array(feature_list)
+
+    # random split train and test
+    indices = np.arange(len(feature_list))
+    np.random.shuffle(indices)
+    train_ind, test_ind = indices[:int(0.6*len(feature_list))], indices[int(0.6*len(feature_list)):]
+
+    # split features in training and testing
+    train_features = feature_list[train_ind]
+    test_features = feature_list[test_ind]
+    print('testfeat cnt', len(test_features))
+
+    # get the labels
+    labels = pkl.load(open('../../Data/Pickle/feat_label_map', 'rb'))
+    labels = np.array([labels[key] for key in labels.keys()])
+    temp_label = []
+    for i in range(feature_list.shape[0]):
+        if i == feature_list.shape[0]:
+            t = labels[:-90, :]
+        t = labels[128*i:128*(i+1), :]
+        temp_label.append(t)
+    labels = np.array(temp_label)
+
+    # split labels in training and testing
+    train_labels = []
+    for i in train_ind:
+        train_labels.append(labels[i])
+    train_labels = np.array(train_labels)
+    test_labels = []
+    for i in test_ind:
+        test_labels.append(labels[i])
+    test_labels = np.array(test_labels)
+
+    # list for correct predictions
+    pred_cnt = []
+
+    # for each label
+    for label_ind in range(9):
+        print("Classfier for label", label_ind)
+        # define the classifier with C value
+        clf = SVM(C=c)
+        cnt = 0
+
+        # train the classifier
+        for i in range(train_features.shape[0]):
+            print("Training batch", cnt + 1)
+            data = pkl.load(open('../../Data/Pickle/flatten_features/' + train_features[i], 'rb'))
+            labels_batch = train_labels[i][:, label_ind]
+            clf.fit(data, labels_batch)
+            cnt += 1
+
+        # testing parameters
+        correct = 0
+        total = 0
+        cnt = 0
+
+        # test the classifier
+        for i in range(test_features.shape[0]):
+            print("Testing batch", cnt + 1)
+            test_data = pkl.load(open('../../Data/Pickle/flatten_features/'+test_features[i], 'rb'))
+            label_test = test_labels[i][:, label_ind]
+            y_predict = clf.predict(test_data)
+            total += len(y_predict)
+            correct += np.sum(y_predict == label_test)
+            print("correct", correct)
+            cnt += 1
+
+        print("%d out of %d predictions correct" % (correct, total))
+        pred_cnt.append(correct)
+
+    print('pred_cnt',pred_cnt)
+    pkl.dump(pred_cnt, open('../../Data/Pickle/svm/pred_cnt_'+str(c)+'.p', 'wb'))
+    plot(pred_cnt, total, c)
+
+
+def plot(pred_cnt, total, C):
+    y_pos = range(9)
+    labels = ['good_for_lunch', 'good_for_dinner', 'takes_reservations', 'outdoor_seating', 'restaurant_is_expensive', 'has_alcohol', 'has_table_service', 'ambience_is_classy', 'good_for_kids']
+    pred_cnt = np.array(pred_cnt)/total
+    plt.bar(y_pos, pred_cnt, align='center')
+    plt.xticks(y_pos, labels)
+    plt.subplots_adjust(bottom=0.15)
+    plt.ylabel('Accuracy')
+    plt.title('SVM with C='+str(C))
+    plt.savefig('../../Data/Plots/svm_bar_'+str(C)+'.png')
+    plt.show()
+
+def plot_all():
+    C = [10, 50, 100, 200, 500, 1000]
+    labels = ['good_for_lunch', 'good_for_dinner', 'takes_reservations', 'outdoor_seating', 'restaurant_is_expensive',
+              'has_alcohol', 'has_table_service', 'ambience_is_classy', 'good_for_kids']
+    color = ['brown', 'r', 'tomato', 'lightsalmon', 'saddlebrown', 'linen']
+    total = 734 * 128
+    y_pos = np.arange(9)
+    w = 0.09
+    p = []
+    for i in range(len(C)):
+        pred_cnt = np.array(pkl.load(open('../../Data/Pickle/svm/pred_cnt_' + str(C[i]) + '.p', 'rb')))
+        pred_cnt = np.array(pred_cnt) / total
+        p.append(plt.bar(y_pos + (w * (i + 1)), pred_cnt, width=w, color=color[i]))
+    plt.xticks(y_pos + w * 3, labels)
+    plt.subplots_adjust(bottom=0.15)
+    plt.ylabel('Accuracy')
+    plt.title('Soft Margin SVM')
+    plt.legend((p), (C), title='C')
+    plt.savefig('../../Data/Plots/svm_bar_'+str(C)+'.png')
+    plt.show()
+
 if __name__ == "__main__":
+    C = [10, 50, 100, 200, 500, 1000]
+    total = 734 * 128
+    for c in C:
+        print("C:",c)
+        soft_classifier(c)
+        # pred_cnt = np.array(pkl.load(open('../../Data/Pickle/svm/pred_cnt_'+str(C[i])+'.p', 'rb')))
+        # plot(pred_cnt, total, c)
 
-    def test_linear():
-        data = pkl.load(open('../../Data/Pickle/flatten_features/feat_vgg16_1', 'rb'))
-        labels = pkl.load(open('../../Data/Pickle/feat_label_map', 'rb'))
-        labels = np.array([labels[key] for key in labels.keys() if key in range(128)])
-        labels = labels[:, 0]
-        # X1, y1, X2, y2 = gen_lin_separable_overlap_data()
-        # X_train, y_train = split_train(X1, y1, X2, y2)
-        # X_test, y_test = split_test(X1, y1, X2, y2)
-        print(data.shape)
-        print(labels.shape)
-
-        clf = SVM()
-        clf.fit(data, labels)
-
-        y_predict = clf.predict(data)
-        correct = np.sum(y_predict == labels)
-        print("%d out of %d predictions correct" % (correct, len(y_predict)))
-
-
-    def test_soft():
-        data = pkl.load(open('../../Data/Pickle/flatten_features/feat_vgg16_1', 'rb'))
-        labels = pkl.load(open('../../Data/Pickle/feat_label_map', 'rb'))
-        labels = np.array([labels[key] for key in labels.keys() if key in range(128)])
-        labels = labels[:, 0]
-        labels = np.where(labels == 0, -1, labels)
-        print(labels)
-        print("data.shape",data.shape)
-        # X1, y1, X2, y2 = gen_lin_separable_overlap_data()
-        # X_train, y_train = split_train(X1, y1, X2, y2)
-        # X_test, y_test = split_test(X1, y1, X2, y2)
-        print(data.shape)
-        print(labels.shape)
-
-        clf = SVM(C=1000.1)
-        clf.fit(data, labels)
-
-        y_predict = clf.predict(data)
-        correct = np.sum(y_predict == labels)
-        print("%d out of %d predictions correct" % (correct, len(y_predict)))
-
-    test_linear()
-    # test_soft()
